a4ddc8db795557fff2f00938e0e0efc0ce9e1003
Matej <tech@vronk.net>
2020-05-25 21:55:28 +0200

    added two tables aggregating by format (altogether and per top-collection)

--- src/Model/DashboardModel.php
+++ src/Model/DashboardModel.php
@@ -28,8 +28,10 @@ class DashboardModel  {
 			) t_class 
 		inner join public.metadata tp on t_class.id =tp.id
 		group by t_class.value, tp.property",
-		"topcollections"=>"select rootids.rootid, min(m_title.value), count(rel.id), max(rel.n), sum(m_rawsize.value_n ),
-				(select value_n from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasBinarySize' and id = rootids.rootid) bsize
+		"topcollections"=>"SELECT rootids.rootid topcoll_id, 
+					(select value from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle' and id = rootids.rootid limit 1) title,
+					count(rel.id) count_items, max(rel.n), sum(m_rawsize.value_n ) sum_size_items,
+					(select value_n from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasBinarySize' and id = rootids.rootid) bsize
 				from (select DISTINCT(r.id) as rootid
 					from metadata as m
 					left join relations as r on r.id = m.id
@@ -45,11 +47,32 @@ class DashboardModel  {
 							and r.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
 					)
 				) as rootids, public.get_relatives(rootid,'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf') rel
-				left join metadata m_title on m_title.id = rel.id
-					and m_title.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle'
 				left join metadata m_rawsize on m_rawsize.id = rel.id
 					and m_rawsize.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasRawBinarySize'
-				group by rootids.rootid"
+				group by rootids.rootid, title",
+		"formats"=>"select value as format, count(*) from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasFormat'
+	group by value"
+	"formatspercollection"=>"SELECT rootids.rootid topcoll_id, 
+		(select value from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle' and id = rootids.rootid limit 1) title,
+		m_format.value as format, count(m_format.id) as count			
+		from (select DISTINCT(r.id) as rootid
+					from metadata as m
+					left join relations as r on r.id = m.id
+					where
+					m.property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' 
+					and m.value = 'https://vocabs.acdh.oeaw.ac.at/schema#Collection'
+					and r.property != 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+					and r.id NOT IN ( 
+						SELECT DISTINCT(r.id) from metadata as m left join relations as r on r.id = m.id
+						where
+							m.property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' 
+							and m.value = 'https://vocabs.acdh.oeaw.ac.at/schema#Collection'
+							and r.property = 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf'
+					)
+				) as rootids, public.get_relatives(rootid,'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf') rel
+				left join metadata m_format on m_format.id = rel.id
+					and m_format.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasFormat'
+				group by rootids.rootid, title, m_format.value"
 
  	);
 
