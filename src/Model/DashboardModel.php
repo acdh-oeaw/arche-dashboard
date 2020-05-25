006177ee846e2bdaa1c0e4f097200b2e0828370c
Matej <tech@vronk.net>
2020-05-25 22:36:40 +0200

    added size-sum for format overviews

--- src/Model/DashboardModel.php
+++ src/Model/DashboardModel.php
@@ -28,7 +28,7 @@ class DashboardModel  {
 			) t_class 
 		inner join public.metadata tp on t_class.id =tp.id
 		group by t_class.value, tp.property",
-		"topcollections"=>"SELECT rootids.rootid topcoll_id, 
+		"topcollections"=>"SELECT rootids.rootid id, 
 					(select value from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle' and id = rootids.rootid limit 1) title,
 					count(rel.id) count_items, max(rel.n), sum(m_rawsize.value_n ) sum_size_items,
 					(select value_n from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasBinarySize' and id = rootids.rootid) bsize
@@ -50,11 +50,14 @@ class DashboardModel  {
 				left join metadata m_rawsize on m_rawsize.id = rel.id
 					and m_rawsize.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasRawBinarySize'
 				group by rootids.rootid, title",
-		"formats"=>"select value as format, count(*) from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasFormat'
-	group by value",
-	"formatspercollection"=>"SELECT rootids.rootid topcoll_id, 
+		"formats"=>"select mf.value as format, count(distinct mf.id) as cnt_format, count(ms.id) as cnt_size, sum(ms.value_n )  from metadata mf
+join metadata ms on mf.id=ms.id
+where mf.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasFormat'
+and ms.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasRawBinarySize'
+	group by mf.value",
+	"formatspercollection"=>"SELECT rootids.rootid id, 
 		(select value from metadata where property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle' and id = rootids.rootid limit 1) title,
-		m_type.value as type, m_format.value as format, count(rel.id) as count			
+		m_type.value as type, m_format.value as format, count(rel.id) as count, sum(m_size.value_n ) as sum_size			
 		from (select DISTINCT(r.id) as rootid
 					from metadata as m
 					left join relations as r on r.id = m.id
@@ -74,6 +77,8 @@ class DashboardModel  {
 					and m_format.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasFormat'
 				left join metadata m_type on m_type.id = rel.id
 					and m_type.property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+				left join metadata m_size on m_size.id = rel.id
+					and m_size.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasRawBinarySize'
 				group by rootids.rootid, title, m_format.value, m_type.value"
 
  	);
