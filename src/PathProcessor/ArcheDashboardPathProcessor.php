bf238caf9428aacf9442d75738127aa05f783c33
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2020-08-04 15:50:02 +0200

    property detail view added

--- src/PathProcessor/ArcheDashboardPathProcessor.php
+++ src/PathProcessor/ArcheDashboardPathProcessor.php
@@ -22,6 +22,18 @@ class ArcheDashboardPathProcessor implements InboundPathProcessorInterface
             return "/dashboard-property/$names";
         }
         
+        if (strpos($path, '/dashboard-detail-api/') === 0) {
+            $names = preg_replace('|^\/dashboard-detail-api\/|', '', $path);
+            if (strpos($names, 'https:/') === 0) {
+                $names = str_replace('https:/', 'https://', $names);
+            } else if (strpos($names, 'http:/') === 0) {
+                $names = str_replace('http:/', 'http://', $names);
+            }
+            
+            $names = strtr(base64_encode($names), '+/=', '._-');
+            return "/dashboard-detail-api/$names";
+        }
+        
         return $path;
     }
     
