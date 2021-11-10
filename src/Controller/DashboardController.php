a31a9880a2a2a54ce4abd1c0cb09229beeebaf6f
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2021-11-10 09:47:45 +0100

    no cache added to routes

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,292 @@
+<?php
+
+namespace Drupal\arche_dashboard\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DashboardController extends ControllerBase
+{
+    private $data = array();
+    private $repo;
+    private $model;
+    private $helper;
+
+    public function __construct()
+    {
+        $this->config = drupal_get_path('module', 'acdh_repo_gui') . '/config/config.yaml';
+        $this->repo = \acdhOeaw\arche\lib\Repo::factory($this->config);
+        //setup the dashboard model class
+        $this->model = new \Drupal\arche_dashboard\Model\DashboardModel();
+        $this->helper = new \Drupal\arche_dashboard\Helper\DashboardHelper();
+    }
+
+    /**
+     * Dashboard property count view
+     *
+     * @return array
+     */
+    public function dashboard_detail(string $key = "properties"): array
+    {
+        //generate the view
+        $data = $this->generateView($key);
+
+        if (count($data) > 0) {
+            $cols = get_object_vars($data[0]);
+        } else {
+            $cols = array();
+        }
+
+        /* if the key is the properties then we need to change the # in the url */
+        if ($key == 'properties' || $key == 'classes' || $key == 'classesproperties') {
+            $data = $this->helper->generatePropertyUrl($data);
+        }
+
+        switch ($key) {
+            case 'classes':
+                $detailPageUrl = 'dashboard-class-property';
+                break;
+            case 'formats':
+                $detailPageUrl = 'dashboard-format-property';
+                break;
+
+            default:
+                $detailPageUrl = 'dashboard-property';
+                break;
+        }
+
+        return [
+            '#theme' => 'arche-dashboard-table',
+            '#basic' => $data,
+            '#key' => $key,
+            '#cols' => $cols,
+            '#detailPageUrl' => $detailPageUrl,
+            '#cache' => ['max-age' => 0]
+        ];
+    }
+
+    public function dashboard_format_property_detail(string $property): array
+    {
+        $property = base64_decode($property);
+        $data = $this->model->getFacetDetail('https://vocabs.acdh.oeaw.ac.at/schema#hasFormat', $property);
+
+        if (count($data) > 0) {
+            $cols = get_object_vars($data[0]);
+        } else {
+            $cols = array();
+        }
+
+        // print_r ($cols);
+        return [
+            '#theme' => 'arche-dashboard-table',
+            '#basic' => $data,
+            '#key' => $property,
+            '#cols' => $cols,
+            '#cache' => ['max-age' => 0]
+        ];
+    }
+
+    /**
+     * The rdf:type class properties detail view
+     *
+     * @param string $property
+     * @return array
+     */
+    public function dashboard_class_property_detail(string $property): array
+    {
+        $property = base64_decode($property);
+        $data = $this->model->getFacetDetail('http://www.w3.org/1999/02/22-rdf-syntax-ns#type', $property);
+
+        if (count($data) > 0) {
+            $cols = get_object_vars($data[0]);
+        } else {
+            $cols = array();
+        }
+
+        // print_r ($cols);
+        return [
+            '#theme' => 'arche-dashboard-table',
+            '#basic' => $data,
+            '#key' => $property,
+            '#cols' => $cols,
+            '#cache' => ['max-age' => 0]
+        ];
+    }
+
+    /**
+     * Dashboard property count distinct values  view
+     *
+     * @return array
+     */
+    public function dashboard_property_detail(string $property): array
+    {
+        $property = base64_decode($property);
+        $data = $this->model->getFacet($property);
+
+        if (count($data) > 0) {
+            $cols = get_object_vars($data[0]);
+        } else {
+            $cols = array();
+        }
+      
+        return [
+            '#theme' => 'arche-dashboard-table',
+            '#basic' => $data,
+            '#key' => $property,
+            '#cols' => $cols,
+            '#cache' => ['max-age' => 0]
+        ];
+    }
+
+    /**
+     * The Dashboard Main Menu View
+     *
+     * @return array
+     */
+    public function dashboard_overview(): array
+    {
+        return [
+            '#theme' => 'arche-dashboard-overview',
+            '#cache' => ['max-age' => 0]
+        ];
+    }
+
+    /**
+     * The basic view generation function, which will handle the sql queries based
+     * on the passed property
+     *
+     * @param type $key
+     * @return array
+     */
+    public function generateView($key): array
+    {
+
+        //get the data from the DB
+        $this->data = $this->model->getViewData($key);
+        //pass the DB result to the Object generate functions
+
+        return $this->data;
+    }
+
+    public function generateHeaders($key): array
+    {
+        return $this->model->getHeaders($key);
+    }
+
+    /**
+     * The properties deatil view
+     *
+     * @param string $property
+     * @return Response
+     */
+    public function dashboard_property_detail_api(string $property): Response
+    {
+        $property = base64_decode($property);
+        //get the value the value after the last /
+        $value = substr($property, strrpos($property, '/') + 1);
+        $property = str_replace('/' . $value, '', $property);
+
+        $data = $this->model->getFacetDetail($property, $value);
+
+        if (count($data) > 0) {
+            $cols = get_object_vars($data[0]);
+        } else {
+            $cols = array();
+        }
+
+        $build = [
+            '#theme' => 'arche-dashboard-table-detail',
+            '#basic' => $data,
+            '#key' => $property,
+            '#keyValue' => $value,
+            '#cols' => $cols,
+            '#cache' => ['max-age' => 0]
+        ];
+
+        return new Response(render($build));
+    }
+
+    /**
+     * The properties deatil view
+     *
+     * @param string $property
+     * @return Response
+     */
+    public function dashboard_dissemination_services_list()
+    {
+        $disservHelper = new \Drupal\arche_dashboard\Helper\DisseminationServiceHelper();
+        $data = $disservHelper->getDissServices();
+        return [
+            '#theme' => 'arche-dashboard-disserv-table',
+            '#cache' => ['max-age' => 0]
+        ];
+    }
+
+    /**
+     * Dissemination services list api call for the datatable
+     * @return Response
+     */
+    public function getDisseminationServiceApi(): Response
+    {
+        $data = array();
+
+        $disservHelper = new \Drupal\arche_dashboard\Helper\DisseminationServiceHelper();
+        $data = $disservHelper->getDissServices();
+        
+        $response = new Response();
+        $response->setContent(json_encode(array("data" => $data)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+    
+    /**
+     * Dissemination service detail page with the basic infos
+     * @param string $id
+     * @return type
+     */
+    public function dashboard_dissemination_services_detail(string $id)
+    {
+        $disservHelper = new \Drupal\arche_dashboard\Helper\DisseminationServiceHelper();
+        $data = $disservHelper->getDissServResourcesById((int)$id);
+      
+        return [
+            '#theme' => 'arche-dashboard-disserv-detail',
+            '#data' => $data,
+            '#cache' => ['max-age' => 0]
+        ];
+    }
+    
+    /**
+     * The matching resource api call for the dissemination service detail datatable
+     * @param string $id
+     * @param int $limit
+     * @param int $offset
+     * @return Response
+     */
+    public function getDisseminationServiceMatchingResourcesApi(string $id): Response
+    {
+        $offset = (empty($_POST['start'])) ? 0 : $_POST['start'];
+        $limit = (empty($_POST['length'])) ? 10 : $_POST['length'];
+        $draw = (empty($_POST['draw'])) ? 0 : $_POST['draw'];
+        
+        $data = array();
+        $disservHelper = new \Drupal\arche_dashboard\Helper\DisseminationServiceHelper();
+        $data = $disservHelper->getDissServResourcesById((int)$id);
+        $matching = $data->getMatchingResources((int)$limit, (int)$offset);
+       
+        $response = new Response();
+        $response->setContent(
+            json_encode(
+                array(
+                    "aaData" => $matching,
+                    "iTotalRecords" => $data->getCount(),
+                    "iTotalDisplayRecords" => $data->getCount(),
+                    "draw" => intval($draw),
+                )
+            )
+        );
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+}
---
+++ src/Model/DashboardModel.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Drupal\arche_dashboard\Model;
+
+/**
+ * Description of DashboardModel
+ *
+ * @author norbertczirjak
+ */
+class DashboardModel
+{
+    private $repodb;
+
+    private $queries = array(
+        "properties"            =>  "SELECT * FROM gui.dash_properties_func();",
+        "classes"               =>  "SELECT * FROM gui.dash_classes_func();",
+        "classesproperties"     =>  "SELECT * FROM gui.dash_classes_properties_func();",
+        "topcollections"        =>  "SELECT * FROM gui.dash_topcollections_func();",
+        "formats"               =>  "SELECT * FROM gui.dash_formats_func();",
+    "formatspercollection"  =>  "SELECT * FROM gui.dash_formatspercollection_func();"
+    );
+   
+    public function __construct()
+    {
+        //set up the DB connections
+        \Drupal\Core\Database\Database::setActiveConnection('repo');
+        $this->repodb = \Drupal\Core\Database\Database::getConnection('repo');
+    }
+    
+    /**
+     * Generate the sql data
+     * @param string $key
+     * @return array
+     */
+    public function getViewData(string $key="properties"): array
+    {
+        if (array_key_exists($key, $this->queries)) {
+            $queryStr = $this->queries[$key];
+        } else { # default query, but better to return empty result, or error message
+            $queryStr = "
+            SELECT 
+                property as key, count(*) as cnt
+            from public.metadata_view 
+            group by property";
+        }
+        try {
+            $query = $this->repodb->query($queryStr);
+            $return = $query->fetchAll();
+            
+            $this->changeBackDBConnection();
+            return $return;
+        } catch (Exception $ex) {
+            \Drupal::logger('arche_dashboard')->notice($ex->getMessage());
+            return array();
+        } catch (\Drupal\Core\Database\DatabaseExceptionWrapper $ex) {
+            \Drupal::logger('arche_dashboard')->notice($ex->getMessage());
+            return array();
+        }
+    }
+       
+    /**
+     * Retrieve the data: faceting: distinct values of a property
+     * @param string $property
+     * @return array
+     */
+    public function getFacet(string $property): array
+    {
+        try {
+            $query = $this->repodb->query(
+                "SELECT * FROM gui.dash_get_facet_func(:property);
+                ",
+                array(
+                    ':property' => $property
+                )
+            );
+            $return = $query->fetchAll();
+            
+            $this->changeBackDBConnection();
+            return $return;
+        } catch (Exception $ex) {
+            \Drupal::logger('arche_dashboard')->notice($ex->getMessage());
+            return array();
+        } catch (\Drupal\Core\Database\DatabaseExceptionWrapper $ex) {
+            \Drupal::logger('arche_dashboard')->notice($ex->getMessage());
+            return array();
+        }
+    }
+    
+    /**
+     * Retrieve the faceting detail data
+     * @param string $property
+     * @param string $value
+     * @return array
+     */
+    public function getFacetDetail(string $property, string $value): array
+    {
+        try {
+            $query = $this->repodb->query(
+                "select mv.id, 
+                (select mv2.value from metadata_view as mv2 where mv2.id = mv.id and mv2.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle' limit 1) as title,
+                (select mv2.value from metadata_view as mv2 where mv2.id = mv.id and mv2.property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' limit 1) as type
+                from metadata_view as mv
+                where 
+                mv.value = :value and mv.property = :property;
+                ",
+                array(
+                    ':value' => $value,
+                    ':property' => $property
+                )
+            );
+            $return = $query->fetchAll();
+            
+            $this->changeBackDBConnection();
+            return $return;
+        } catch (Exception $ex) {
+            \Drupal::logger('arche_dashboard')->notice($ex->getMessage());
+            return array();
+        } catch (\Drupal\Core\Database\DatabaseExceptionWrapper $ex) {
+            \Drupal::logger('arche_dashboard')->notice($ex->getMessage());
+            return array();
+        }
+    }
+    
+    
+    /**
+     * Count the selected dissemination service matching resources
+     * @param object $sql
+     * @return int
+     */
+    public function countAllMatchingResourcesForDisseminationService(object $sql): int
+    {
+        try {
+            $query = $this->repodb->query(
+                $sql->query,
+                $sql->param
+            );
+            
+            $return = $query->fetchObject();
+            $this->changeBackDBConnection();
+            return (int)$return->count;
+        } catch (Exception $ex) {
+            \Drupal::logger('arche_dashboard')->notice($ex->getMessage());
+            return 0;
+        } catch (\Drupal\Core\Database\DatabaseExceptionWrapper $ex) {
+            \Drupal::logger('arche_dashboard')->notice($ex->getMessage());
+            return 0;
+        }
+    }
+ 
+    public function changeBackDBConnection()
+    {
+        \Drupal\Core\Database\Database::setActiveConnection();
+    }
+}
